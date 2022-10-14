8966f879b95730dbf68159b4b3af0e1a2f5d23c1
Alex Rintt <alexrintt@gmail.com>
2022-10-14 18:20:58 -0300

    Make `createSchemaCustomization` async

--- packages/gatsby-source-github-graphql/create-schema-customization.js
+++ packages/gatsby-source-github-graphql/create-schema-customization.js
@@ -1,17 +1,17 @@
-const { getPluginNodeTypes } = require(`./github-graphql-defs`)
+const { getPluginNodeTypes } = require(`./github-graphql-defs`);
 
-module.exports = (gatsbyNodeApis, pluginOptions) => {
-  const pluginNodeTypes = getPluginNodeTypes(pluginOptions.pluginNodeTypes)
+module.exports = async (gatsbyNodeApis, pluginOptions) => {
+  const pluginNodeTypes = getPluginNodeTypes(pluginOptions.pluginNodeTypes);
 
-  const createTypes = gatsbyNodeApis.actions.createTypes
+  const createTypes = gatsbyNodeApis.actions.createTypes;
 
   // All GitHub API models should have at least their ID.
   const typeDefs = Object.values(pluginNodeTypes)
-    .map(type => `type ${type} implements Node { githubId: String! }`)
-    .join(`\n`)
+    .map((type) => `type ${type} implements Node { githubId: String! }`)
+    .join(`\n`);
 
   // Default plugin schema types
-  createTypes(typeDefs)
+  createTypes(typeDefs);
 
   if (pluginOptions.generateOptimizedGitHubUserAvatarUrl !== false) {
     // Default image optimization for GitHub user model.
@@ -19,31 +19,31 @@ module.exports = (gatsbyNodeApis, pluginOptions) => {
       type ${pluginNodeTypes.USER} implements Node {
         avatarUrlSharpOptimized: File @link(from: "fields.avatarUrlSharpOptimized")
       }
-    `
-    createTypes(userTypeDef)
+    `;
+    createTypes(userTypeDef);
   }
 
-  const { plugins = [] } = pluginOptions
+  const { plugins = [] } = pluginOptions;
 
   // allow subplugins create schema customizations
   for (const plugin of plugins) {
-    const resolvedPlugin = plugin.module
+    const resolvedPlugin = plugin.module;
 
-    const createSchemaCustomization = resolvedPlugin.createSchemaCustomization
+    const createSchemaCustomization = resolvedPlugin.createSchemaCustomization;
 
     if (typeof createSchemaCustomization === `function`) {
       createSchemaCustomization(gatsbyNodeApis, {
         ...plugin.pluginOptions,
         pluginNodeTypes,
-      })
+      });
     }
   }
 
   // allow end-users (developers) create schema customizations
   if (typeof pluginOptions.createSchemaCustomization === `function`) {
-    pluginOptions.createSchemaCustomization(gatsbyNodeApis, {
+    await pluginOptions.createSchemaCustomization(gatsbyNodeApis, {
       ...pluginOptions,
       pluginNodeTypes,
-    })
+    });
   }
-}
+};
